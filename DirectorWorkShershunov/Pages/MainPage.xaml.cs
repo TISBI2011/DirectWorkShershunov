42cd2c5e4bab9a4f6ecbc36a4c87b0ee645da361
ИС201-1 <IS201-1@tisbi.ru>
2023-12-05 16:03:41 +0300

    Молитва

---
+++ DirectorWorkShershunov/Pages/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DirectorWorkShershunov.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BKommedia_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BHoror_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BDrama_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BMelodrama_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BFentezi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
